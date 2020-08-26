9ccef2ce6ee75cba742354972fbbb5d7ab8ee805
Seghir Nadir <nadir.seghir@gmail.com>
2020-08-26 14:01:42 +0100

    fix broken tests

--- tests/utils/index.js
+++ tests/utils/index.js
@@ -1,2 +1,3 @@
 export { findToggleWithLabel } from './find-toggle-with-label';
 export { visitBlockPage } from './visit-block-page';
+export { findElementWithText } from './find-element-with-text';
